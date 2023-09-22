2720058f3a1975fefaa44e2c1151e06403ced911
Dylan McCall <dylan@endlessos.org>
2023-09-22 11:04:28 -0700

    Interpret yes / no values for environment variables
    In kolibri_app.globals, use a function similar to "strtobool" in order
    to interpret environment variables that expect yes / no values. This
    will make the application behave more predictably, and ensures that
    types are consistent.

--- src/kolibri_app/globals.py
+++ src/kolibri_app/globals.py
@@ -7,19 +7,20 @@ import typing
 from pathlib import Path
 
 from . import config
+from .utils import getenv_as_bool
 
 logger = logging.getLogger(__name__)
 
-APP_DEVELOPER_EXTRAS = os.environ.get(
+APP_DEVELOPER_EXTRAS = getenv_as_bool(
     config.PROFILE_ENV_PREFIX + "APP_DEVELOPER_EXTRAS",
-    config.BUILD_PROFILE == "development",
+    default=config.BUILD_PROFILE == "development",
 )
 
-APP_DISABLE_AUTOMATIC_LOGIN = os.environ.get(
+APP_AUTOMATIC_LOGIN = getenv_as_bool(
     config.PROFILE_ENV_PREFIX + "APP_DISABLE_AUTOMATIC_LOGIN"
 )
 
-APP_DISABLE_AUTOMATIC_PROVISION = os.environ.get(
+APP_AUTOMATIC_PROVISION = getenv_as_bool(
     config.PROFILE_ENV_PREFIX + "APP_DISABLE_AUTOMATIC_PROVISION"
 )
 
