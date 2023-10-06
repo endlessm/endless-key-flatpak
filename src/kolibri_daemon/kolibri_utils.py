e1275e75d92e51107c8ebd6c2a233067dd9bd9f9
Jian-Hong Pan <jhp@endlessos.org>
2023-10-06 12:11:49 +0800

    Add KOLIBRI_PROJECT environment variable
    Helps: https://github.com/endlessm/kolibri-explore-plugin/issues/868

--- src/kolibri_daemon/kolibri_utils.py
+++ src/kolibri_daemon/kolibri_utils.py
@@ -59,6 +59,7 @@ def init_kolibri(**kwargs):
 
 def _init_kolibri_env():
     os.environ["DJANGO_SETTINGS_MODULE"] = "kolibri_app.kolibri_settings"
+    os.environ["KOLIBRI_PROJECT"] = "endless-key-flatpak"
 
     # Kolibri defaults to a very large thread pool. Because we expect this
     # application to be used in a single user environment with a limited
